2cdbca9c5c84a734418357ff245508eb1e8c0d78
Shaw <eastwoodshaw@gmail.com>
2025-03-15 17:22:27 +0000

    feat: update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,28 +1,24 @@
 <x-layouts.app :title="__('Dashboard')">
 	<div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
 		<div class="grid auto-rows-min gap-4 md:grid-cols-3">
-			<div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-				<div class="p-6">
-					<flux:subheading size="lg">Total Wines</flux:subheading>
-					<flux:heading size="xl">09111123</flux:heading>
-				</div>
-			</div>
-			<div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-				<div class="p-6">
-					<flux:subheading size="lg">Total Users</flux:subheading>
-					<flux:heading size="xl">123</flux:heading>
-				</div>
-			</div>
-			<div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-				<div class="p-6">
-					<flux:subheading size="lg">Total Reviews</flux:subheading>
-					<flux:heading size="xl">19123</flux:heading>
-				</div>
-			</div>
+			<flux:card>
+				<flux:subheading size="lg">Total Bottles</flux:subheading>
+				<flux:heading size="xl">{{ \App\Models\Bottle::count() }}</flux:heading>
+			</flux:card>
+
+			<flux:card>
+				<flux:subheading size="lg">Total Purchases</flux:subheading>
+				<flux:heading size="xl">{{ \App\Models\Purchase::count() }}</flux:heading>
+			</flux:card>
+
+			<flux:card>
+				<flux:subheading size="lg">Total Ratings</flux:subheading>
+				<flux:heading size="xl">{{ \App\Models\Rating::count() }}</flux:heading>
+			</flux:card>
 		</div>
 
-		<div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+		<flux:card class="h-full flex-1">
 			<livewire:dashboard.wine-list />
-		</div>
+		</flux:card>
 	</div>
 </x-layouts.app>
