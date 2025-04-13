77e0b9b4dab9bf59cd3434a5f4afff84dbc8757e
Shaw <eastwoodshaw@gmail.com>
2025-04-14 00:19:45 +0100

    feat(purchase): form phone view

--- resources/views/livewire/purchase/add-update.blade.php
+++ resources/views/livewire/purchase/add-update.blade.php
@@ -20,27 +20,39 @@
 			@endif
 
 			@empty($bottle)
-			<div class="grid grid-cols-[1fr_8rem] gap-6 items-end mb-6">
-				<flux:select
-					label="Bottle"
-					badge="Required"
-					variant="listbox"
-					searchable
-					wire:model="form.bottle_id"
-					class="[&>button>ui-selected>div]:w-full!"
-				>
-					@foreach($this->bottles as $bottle)
-						<flux:select.option :value="$bottle->id">
-							<div class="flex gap-3 w-full">
-								<flux:subheading class="tabular-nums">{{ $bottle->vintage }}</flux:subheading>
-								<flux:heading class="mt-0! ml-3 mr-auto">{{ $bottle->name }}</flux:heading>
-								<x-bottle.wine-type :wine_type="$bottle->wine_type" size="sm" inset="top bottom" />
-							</div>
-						</flux:select.option>
-					@endforeach
-				</flux:select>
+			<div class="flex gap-3 items-end mb-6">
+				<div class="flex-1">
+					<flux:select
+						label="Bottle"
+						badge="Required"
+						variant="listbox"
+						searchable
+						wire:model="form.bottle_id"
+						class="[&>button>ui-selected>div]:w-full!"
+					>
+						@foreach($this->bottles as $bottle)
+							<flux:select.option :value="$bottle->id">
+								<div class="flex gap-3 w-full">
+									<flux:subheading class="tabular-nums">{{ $bottle->vintage }}</flux:subheading>
+									<flux:heading class="mt-0! ml-3 mr-auto">{{ $bottle->name }}</flux:heading>
+									<x-bottle.wine-type :wine_type="$bottle->wine_type" size="sm" inset="top bottom" />
+								</div>
+							</flux:select.option>
+						@endforeach
+					</flux:select>
+				</div>
 
-				<flux:button wire:click="$dispatch('create-bottle')" class="mt-6">
+				<flux:button
+					class="md:hidden"
+					icon="plus"
+					wire:click="$dispatch('create-bottle')"
+				/>
+
+				<flux:button
+					class="w-28 max-md:hidden"
+					icon="plus"
+					wire:click="$dispatch('create-bottle')"
+				>
 					Add New
 				</flux:button>
 			</div>
@@ -60,17 +72,21 @@
 			</div>
 
 
-			<div class="grid grid-cols-[1fr_3fr] gap-6 w-full my-6 items-end">
-				<flux:input.group label="Cost" badge="Required">
-					<flux:input.group.prefix>£</flux:input.group.prefix>
+			<div class="flex gap-6 w-full my-6 items-end">
+				<div class="max-w-40">
+					<flux:input.group label="Cost" badge="Required">
+						<flux:input.group.prefix>£</flux:input.group.prefix>
 
-					<flux:input wire:model="form.cost" />
-				</flux:input.group>
+						<flux:input wire:model="form.cost" />
+					</flux:input.group>
+				</div>
 
-				<flux:input
-					label="Store"
-					wire:model="form.store"
-				/>
+				<div class="flex-1">
+					<flux:input
+						label="Store"
+						wire:model="form.store"
+					/>
+				</div>
 			</div>
 
 			<flux:editor
