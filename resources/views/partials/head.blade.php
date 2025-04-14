b06c7b960274adf056abc759366fd3c3895c8700
Shaw <eastwoodshaw@gmail.com>
2025-04-14 01:31:58 +0100

    feat: favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,9 @@
 
 <title>{{ $title ?? 'Pinovino' }}</title>
 
+<link rel="icon" href="favicon.ico" sizes="16x16">
+<link rel="icon" href="favicon.svg" type="image/svg+xml" sizes="any">
+
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
