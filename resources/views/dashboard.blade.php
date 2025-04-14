11cf4fbfbf30d6e7dc72c438c0c56eec19219975
Shaw <eastwoodshaw@gmail.com>
2025-04-14 01:17:07 +0100

    cosmetic(dash): remove second card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,8 +2,8 @@
 	<div class="flex h-full w-full flex-1 flex-col gap-3 rounded-xl">
 		<livewire:dashboard.stats lazy />
 
-		<flux:card class="h-full flex-1">
+		<div class="h-full flex-1 mt-6">
 			<livewire:dashboard.wine-list />
-		</flux:card>
+		</div>
 	</div>
 </x-layouts.app>
